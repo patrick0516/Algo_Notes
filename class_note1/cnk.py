396a7db45404f55237cb5b2449a1c44840b06ce8
Zhou Chen En <109636871+patrick0516@users.noreply.github.com>
2024-06-22 18:20:53 +0800

    Create cnk.py

---
+++ class_note1/cnk.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    p = 1
+    #階乘計算
+    for i in range(n):
+        p = p * (i+1)
+    return p
+
+#C(n,l)公式
+def c(n, k):
+    return factorial(n) / (factorial(k)*factorial(n-k))
+
+print("c(5,2)=", c(5,2))
+print("c(7,3)=", c(7,3))
+print("c(12,5)=", c(12,5))
+print("c(60,30)=", c(60,30)) 
